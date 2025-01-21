1c76e97218456a65976362d278e9b7c3a22b574a
mohit <mohit.paudyal@gmail.com>
2025-01-21 11:04:21 -0600

    329 - Update dev script to support token
    - Updated dev script to support custom token
    
    Added .env file changes so that now all jwt tokens
    are custom for the server they are under. Users signed with the selected
    token must use the same token to decode the key.
    
    Closes #329

--- apilayer/config/common.go
+++ apilayer/config/common.go
@@ -7,9 +7,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// the auth key required to decode any request to mashed api
-var BASE_LICENSE_KEY = []byte("2530d6a4-5d42-4758-b331-2fbbfed27bf9")
-
 const CTO_USER = "community_test"
 const CEO_USER = "ceo_test"
 
